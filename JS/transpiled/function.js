cc9e2bf544372e0873411992f0d1dd947802f387
Dakota M <dakota.paige@hotmail.com>
2018-04-07 19:51:30 -0700

    Added fonts and started on the scroll feature with the cat walking across the page

--- JS/transpiled/function.js
+++ JS/transpiled/function.js
@@ -10,4 +10,29 @@ $(document).ready(function () {
       $('nav').removeClass('banner-header');
     }
   });
+
+  //defines the path for the cat to walk along
+  var catpath = {
+    entry: {
+      autoRotate: true,
+      values: [{ x: 100, y: 0 }, { x: 300, y: 10 }]
+    },
+    leave: {
+      autoRotate: true,
+      values: [{ x: 500, y: 10 }, { x: 600, y: 0
+        // {x: $(window).width() + 300,  y: -100}
+      }]
+    }
+  };
+
+  //init scrollmagic controller
+  var controller = new ScrollMagic.Controller();
+
+  //create tween for scrollmagic
+  var tween = new TimelineMax().add(TweenMax.to($('#cat'), 1.2, { css: { bezier: catpath.entry }, ease: Power1.easeInOut })).add(TweenMax.to($('#cat'), 1.2, { css: { bezier: catpath.leave }, ease: Power1.easeInOut }));
+
+  //build scene for scrollmagic
+  var scene = new ScrollMagic.Scene({ triggerElement: '#trigger', duration: 500 }).setPin('#target').setTween(tween)
+  // .addIndicators({name: "2 (duration: 300"})
+  .addTo(controller);
 });
