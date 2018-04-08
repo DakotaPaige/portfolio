f2766540b46d4e954cda9fe0dc95ac8fe7d6b242
Dakota M <dakota.paige@hotmail.com>
2018-04-07 20:35:32 -0700

    Added smoothscroll

--- JS/transpiled/function.js
+++ JS/transpiled/function.js
@@ -55,4 +55,29 @@ $(document).ready(function () {
   .addTo(controller);
 
   let scene2 = new ScrollMagic.Scene({ triggerElement: '#trigger', duration: 400, offset: 50 }).setTween(tween2).addTo(controller);
+
+  //trying to add in smoothscroll
+  // Select all links with hashes
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]').not('[href="#0"]').click(function (event) {
+    // On-page links
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function () {
+          // Callback after animation
+          // Must change focus!
+
+        });
+      }
+    }
+  });
 });
