10b8ed05e6ef04e694d1f1cd6491f5e12300dd74
Dakota <dakota.paige@hotmail.com>
2018-07-23 21:57:17 -0700

    Fixed bug with JS, updated flickity carousels with images instead of gifs

--- JS/function.js
+++ JS/function.js
@@ -2,37 +2,34 @@ $(document).ready(function() {
   //add class to make header transparent on load
   $('nav').addClass('banner-header');
 
-  AOS.init();
-
   $('.main-carousel').flickity({
     // options
-    cellAlign: 'left',
-    contain: true
+    cellAlign: 'center',
+    resize: true
   });
 
-  $(window).scroll(function(){
-    let currentScreenPosition  = $(document).scrollTop();
+  AOS.init({ offset: -5 });
+
+  $(window).scroll(function() {
+    let currentScreenPosition = $(document).scrollTop();
     if (currentScreenPosition < $('header').height()) {
-      $( 'nav' ).addClass( 'banner-header' );
+      $('nav').addClass('banner-header');
     } else {
-      $( 'nav' ).removeClass( 'banner-header' );
+      $('nav').removeClass('banner-header');
     }
   });
-
+  
   //defines the path for the cat to walk along
   let catpath = {
-    entry : {
+    entry: {
       autoRotate: true,
-      values: [
-        {x: 100,  y: 0},
-        {x: 300,  y: 0}
-      ]
+      values: [{ x: 100, y: 0 }, { x: 300, y: 0 }]
     },
-    leave : {
+    leave: {
       autoRotate: false,
       values: [
-        {x: 350,  y: 0},
-        {x: 350,  y: 0}
+        { x: 350, y: 0 },
+        { x: 350, y: 0 }
         // {x: $(window).width() + 300,  y: -100}
       ]
     }
@@ -101,74 +98,91 @@ $(document).ready(function() {
     'images/cats/cat39.png',
     'images/cats/cat40.png',
     'images/cats/cat41.png'
-
   ];
 
-  let obj = {curImg: 0};
+  let obj = { curImg: 0 };
 
   // create tween for image transition
-  let tween2 = TweenMax.to(obj, 0.1, 
-      {
-        curImg: images.length - 1,
-        roundProps: 'curImg',
-        repeat: 0,
-        immediateRender: true,
-        ease: Linear.easeNone,
-        onUpdate: function() {
-          $('#cat').attr('src', images[obj.curImg]);
-        }
-      });
+  let tween2 = TweenMax.to(obj, 0.1, {
+    curImg: images.length - 1,
+    roundProps: 'curImg',
+    repeat: 0,
+    immediateRender: true,
+    ease: Linear.easeNone,
+    onUpdate: function() {
+      $('#cat').attr('src', images[obj.curImg]);
+    }
+  });
 
   // console.log(tween2);
 
   //create tween for movement for scrollmagic
   let tween = new TimelineMax()
-    .add(TweenMax.to($('#cat'), 1.2,{css:{bezier:catpath.entry}, ease:Power1.easeInOut}))
-    .add(TweenMax.to($('#cat'), 1.2,{css:{bezier:catpath.leave}, ease:Power1.easeInOut}));
+    .add(
+      TweenMax.to($('#cat'), 1.2, {
+        css: { bezier: catpath.entry },
+        ease: Power1.easeInOut
+      })
+    )
+    .add(
+      TweenMax.to($('#cat'), 1.2, {
+        css: { bezier: catpath.leave },
+        ease: Power1.easeInOut
+      })
+    );
 
   //init scrollmagic controller
   let controller = new ScrollMagic.Controller();
 
   //build scene for scrollmagic
-  let scene = new ScrollMagic.Scene({triggerElement:'#trigger', duration: 300})
-                  .setPin('#target')
-                  .setTween(tween)
-                  // .addIndicators({name: "2 (duration: 300"})
-                  .addTo(controller);
-  
-  let scene2 = new ScrollMagic.Scene({triggerElement: '#trigger', duration: 300})
-                   .setTween(tween2)
-                   .addTo(controller);
-                   
-                   
+  let scene = new ScrollMagic.Scene({
+    triggerElement: '#trigger',
+    duration: 300
+  })
+    .setPin('#target')
+    .setTween(tween)
+    // .addIndicators({name: "2 (duration: 300"})
+    .addTo(controller);
+
+  let scene2 = new ScrollMagic.Scene({
+    triggerElement: '#trigger',
+    duration: 300
+  })
+    .setTween(tween2)
+    .addTo(controller);
+
   //smoothscroll
   // Select all links with hashes
   $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top -50
-        }, 1000, function() {
-          // Callback after animation
-          
-        });
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') ===
+          this.pathname.replace(/^\//, '') &&
+        location.hostname === this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate(
+            {
+              scrollTop: target.offset().top - 50
+            },
+            1000,
+            function() {
+              // Callback after animation
+            }
+          );
+        }
       }
-    }
-  });
-  
-})
+    });
+});
--- JS/transpiled/function.js
+++ JS/transpiled/function.js
@@ -1,17 +1,19 @@
+'use strict';
+
 $(document).ready(function () {
   //add class to make header transparent on load
   $('nav').addClass('banner-header');
 
-  AOS.init();
-
   $('.main-carousel').flickity({
     // options
-    cellAlign: 'left',
-    contain: true
+    cellAlign: 'center',
+    resize: true
   });
 
+  AOS.init({ offset: -5 });
+
   $(window).scroll(function () {
-    let currentScreenPosition = $(document).scrollTop();
+    var currentScreenPosition = $(document).scrollTop();
     if (currentScreenPosition < $('header').height()) {
       $('nav').addClass('banner-header');
     } else {
@@ -20,7 +22,7 @@ $(document).ready(function () {
   });
 
   //defines the path for the cat to walk along
-  let catpath = {
+  var catpath = {
     entry: {
       autoRotate: true,
       values: [{ x: 100, y: 0 }, { x: 300, y: 0 }]
@@ -33,18 +35,18 @@ $(document).ready(function () {
     }
   };
 
-  let images = ['images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat4.png', 'images/cats/cat5.png', 'images/cats/cat6.png', 'images/cats/cat7.png', 'images/cats/cat8.png', 'images/cats/cat9.png', 'images/cats/cat10.png', 'images/cats/cat11.png', 'images/cats/cat12.png', 'images/cats/cat13.png', 'images/cats/cat14.png', 'images/cats/cat15.png', 'images/cats/cat16.png', 'images/cats/cat17.png', 'images/cats/cat18.png', 'images/cats/cat19.png', 'images/cats/cat20.png', 'images/cats/cat21.png', 'images/cats/cat22.png', 'images/cats/cat23.png', 'images/cats/cat24.png', 'images/cats/cat25.png', 'images/cats/cat26.png', 'images/cats/cat27.png', 'images/cats/cat28.png', 'images/cats/cat29.png', 'images/cats/cat30.png', 'images/cats/cat31.png', 'images/cats/cat32.png', 'images/cats/cat33.png', 'images/cats/cat34.png', 'images/cats/cat35.png', 'images/cats/cat36.png', 'images/cats/cat37.png', 'images/cats/cat38.png', 'images/cats/cat39.png', 'images/cats/cat40.png', 'images/cats/cat41.png'];
+  var images = ['images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat1.png', 'images/cats/cat2.png', 'images/cats/cat3.png', 'images/cats/cat4.png', 'images/cats/cat5.png', 'images/cats/cat6.png', 'images/cats/cat7.png', 'images/cats/cat8.png', 'images/cats/cat9.png', 'images/cats/cat10.png', 'images/cats/cat11.png', 'images/cats/cat12.png', 'images/cats/cat13.png', 'images/cats/cat14.png', 'images/cats/cat15.png', 'images/cats/cat16.png', 'images/cats/cat17.png', 'images/cats/cat18.png', 'images/cats/cat19.png', 'images/cats/cat20.png', 'images/cats/cat21.png', 'images/cats/cat22.png', 'images/cats/cat23.png', 'images/cats/cat24.png', 'images/cats/cat25.png', 'images/cats/cat26.png', 'images/cats/cat27.png', 'images/cats/cat28.png', 'images/cats/cat29.png', 'images/cats/cat30.png', 'images/cats/cat31.png', 'images/cats/cat32.png', 'images/cats/cat33.png', 'images/cats/cat34.png', 'images/cats/cat35.png', 'images/cats/cat36.png', 'images/cats/cat37.png', 'images/cats/cat38.png', 'images/cats/cat39.png', 'images/cats/cat40.png', 'images/cats/cat41.png'];
 
-  let obj = { curImg: 0 };
+  var obj = { curImg: 0 };
 
   // create tween for image transition
-  let tween2 = TweenMax.to(obj, 0.1, {
+  var tween2 = TweenMax.to(obj, 0.1, {
     curImg: images.length - 1,
     roundProps: 'curImg',
     repeat: 0,
     immediateRender: true,
     ease: Linear.easeNone,
-    onUpdate: function () {
+    onUpdate: function onUpdate() {
       $('#cat').attr('src', images[obj.curImg]);
     }
   });
@@ -52,17 +54,29 @@ $(document).ready(function () {
   // console.log(tween2);
 
   //create tween for movement for scrollmagic
-  let tween = new TimelineMax().add(TweenMax.to($('#cat'), 1.2, { css: { bezier: catpath.entry }, ease: Power1.easeInOut })).add(TweenMax.to($('#cat'), 1.2, { css: { bezier: catpath.leave }, ease: Power1.easeInOut }));
+  var tween = new TimelineMax().add(TweenMax.to($('#cat'), 1.2, {
+    css: { bezier: catpath.entry },
+    ease: Power1.easeInOut
+  })).add(TweenMax.to($('#cat'), 1.2, {
+    css: { bezier: catpath.leave },
+    ease: Power1.easeInOut
+  }));
 
   //init scrollmagic controller
-  let controller = new ScrollMagic.Controller();
+  var controller = new ScrollMagic.Controller();
 
   //build scene for scrollmagic
-  let scene = new ScrollMagic.Scene({ triggerElement: '#trigger', duration: 300 }).setPin('#target').setTween(tween)
+  var scene = new ScrollMagic.Scene({
+    triggerElement: '#trigger',
+    duration: 300
+  }).setPin('#target').setTween(tween)
   // .addIndicators({name: "2 (duration: 300"})
   .addTo(controller);
 
-  let scene2 = new ScrollMagic.Scene({ triggerElement: '#trigger', duration: 300 }).setTween(tween2).addTo(controller);
+  var scene2 = new ScrollMagic.Scene({
+    triggerElement: '#trigger',
+    duration: 300
+  }).setTween(tween2).addTo(controller);
 
   //smoothscroll
   // Select all links with hashes
@@ -70,7 +84,7 @@ $(document).ready(function () {
   // Remove links that don't actually link to anything
   .not('[href="#"]').not('[href="#0"]').click(function (event) {
     // On-page links
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
       // Figure out element to scroll to
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -82,7 +96,6 @@ $(document).ready(function () {
           scrollTop: target.offset().top - 50
         }, 1000, function () {
           // Callback after animation
-
         });
       }
     }
